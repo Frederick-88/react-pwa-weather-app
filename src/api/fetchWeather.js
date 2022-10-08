7c4c5f6e68827eda6d6fda354bf0b3c021d5b261
Chen Frederick <frederickfd88@gmail.com>
2022-10-08 17:33:51 +0700

    API Call Error Handling Updates

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -4,24 +4,19 @@ const URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = "08c63bb12c5a4132f5d570f08f17872d";
 
 export const fetchWeather = async (query) => {
-  let data = {};
-  if (query) {
-    data = await axios.get(URL, {
-      params: {
-        q: query,
-        units: "metric",
-        APPID: API_KEY,
-      },
-    });
-  } else {
-    data = await axios.get(URL, {
-      params: {
-        q: "singapore",
-        units: "metric",
-        APPID: API_KEY,
-      },
-    });
-  }
+  try {
+    let data = {};
 
-  return data;
+    data = await axios.get(URL, {
+      params: {
+        q: query || "singapore",
+        units: "metric",
+        APPID: API_KEY,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    //
+  }
 };
